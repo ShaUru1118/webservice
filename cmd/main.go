fa9f7e559ca09e87740c99bd44b4fbc249b09f01
Albom3 <sargsyan_ae09@mail.ru>
2024-12-18 10:53:00 +0300

    first commit

---
+++ cmd/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ShaUru1118/webservice/internal/application"
+)
+
+func main() {
+	fmt.Println("main: Start")
+	application.StartServer()
+	fmt.Println("main: End")
+}
---
+++ internal/application/application.go
@@ -0,0 +1,27 @@
+package application
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/ShaUru1118/webservice//pkg/simplecalc"
+)
+
+func CalcHandler(w http.ResponseWriter, r *http.Request) {
+	expretion := r.URL.Query().Get("expression")
+	res, errcalc := simplecalc.Calc(expretion)
+	if errcalc != nil {
+		http.Error(w, "Expression is not valid", 422)
+	}
+	fmt.Fprint(w, res)
+}
+
+func StartServer() {
+	fmt.Println("application: Starting Server")
+
+	http.HandleFunc("/api/v1/calculate", CalcHandler)
+
+	http.ListenAndServe(":8080", nil)
+
+	fmt.Println("application: Stopping Server")
+}
