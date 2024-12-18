124581c562ac7ecdb7efb32b9a5286f4adb09e58
Albom3 <sargsyan_ae09@mail.ru>
2024-12-18 12:55:20 +0300

    1255

--- main/webservice.go
+++ main/webservice.go
@@ -6,7 +6,7 @@ import (
 	"github.com/shauru1118/webservice/internal/application"
 )
 
-func main() {
+func Main() {
 	fmt.Println("main: Start")
 	application.StartServer()
 	fmt.Println("main: End")
